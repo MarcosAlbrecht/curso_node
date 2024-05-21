31618d0db8624fb236d2a05f13ae49575add54cb
Marcos Roberto Albrecht <marquinhos.beto@hotmail.com>
2024-05-21 15:21:52 -0300

    insert validation in create user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,3 +1,4 @@
+import { BadRequestException } from '@exceptions/bad-request-exception';
 import { NotFoundException } from '@exceptions/not-found-exception';
 import { PrismaClient } from '@prisma/client';
 import { UserInsertDTO } from './dtos/user-insert.dto';
@@ -14,5 +15,45 @@ export const getUsers = async (): Promise<UserModel[]> => {
 };
 
 export const createUser = async (body: UserInsertDTO): Promise<UserModel> => {
+  const userEmail = await getUserByEmail(body.email).catch(() => undefined);
+
+  if (userEmail) {
+    throw new BadRequestException('Email already exists');
+  }
+
+  const userCpf = await getUserByCpf(body.cpf).catch(() => undefined);
+
+  if (userCpf) {
+    throw new BadRequestException('CPF already exists');
+  }
+
   return prisma.user.create({ data: body });
+};
+
+export const getUserByEmail = async (email: string): Promise<UserModel> => {
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    throw new NotFoundException('User');
+  }
+
+  return user;
+};
+
+export const getUserByCpf = async (cpf: string): Promise<UserModel> => {
+  const user = await prisma.user.findFirst({
+    where: {
+      cpf,
+    },
+  });
+
+  if (!user) {
+    throw new NotFoundException('User');
+  }
+
+  return user;
 };
