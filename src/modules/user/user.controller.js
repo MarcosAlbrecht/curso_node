63dc5bb5af72c4915cd1e48a621e2dd56d4ff873
Marcos Roberto Albrecht <marquinhos.beto@hotmail.com>
2024-05-21 09:47:02 -0300

    insert prisma in project

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,13 +1,16 @@
+import { PrismaClient } from '@prisma/client/edge';
 import { Router } from 'express';
-
 const userRouter = Router();
 
 const router = Router();
 
 userRouter.use('/user', router);
 
-router.get('/', function (req, res) {
-  res.send('Hellow World Now');
+router.get('/', async function (req, res) {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany();
+
+  res.send(users);
 });
 router.get('/:nome', function (req, res) {
   res.send('nome: ' + req.params.nome);
