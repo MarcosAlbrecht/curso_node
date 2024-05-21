d5bdc44228f0018509f3dee71dd0cb41a5748184
Marcos Roberto Albrecht <marquinhos.beto@hotmail.com>
2024-05-21 11:32:55 -0300

    insert type in controllers

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import { UserInsertDTO } from './dtos/user-insert.dto';
 import { createUser, getUsers } from './user.service';
 const userRouter = Router();
 
@@ -6,16 +7,19 @@ const router = Router();
 
 userRouter.use('/user', router);
 
-router.get('/', async (req, res) => {
+router.get('/', async (_, res: Response): Promise<void> => {
   const users = await getUsers();
 
   res.send(users);
 });
 
-router.post('/', async (req, res) => {
-  console.log(req.body);
-  const user = await createUser(req.body);
-  res.send(user);
-});
+router.post(
+  '/',
+  async (req: Request<undefined, undefined, UserInsertDTO>, res: Response): Promise<void> => {
+    console.log(req.body);
+    const user = await createUser(req.body);
+    res.send(user);
+  },
+);
 
 export default userRouter;
