531d2608ead82addb457ad517e385f60db120a2b
Marcos Roberto Albrecht <marquinhos.beto@hotmail.com>
2024-05-22 15:53:09 -0300

    create JWT

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { NotFoundException } from '@exceptions/not-found-exception';
-import { UserModel } from '@modules/user/user.model';
 import { getUserByEmail } from '@modules/user/user.service';
+import { generateToken } from '@utils/auth';
 import { ValidatePassword } from '@utils/password';
+import { AuthModel } from './auth.model';
 import { AuthDTO } from './dtos/auth.dto';
 
-export const validateAuth = async (authDTO: AuthDTO): Promise<UserModel> => {
+export const validateAuth = async (authDTO: AuthDTO): Promise<AuthModel> => {
   const user = await getUserByEmail(authDTO.email);
 
   const isValidPassword = await ValidatePassword(authDTO.password, user.password);
@@ -13,5 +14,5 @@ export const validateAuth = async (authDTO: AuthDTO): Promise<UserModel> => {
     throw new NotFoundException('User');
   }
 
-  return user;
+  return new AuthModel(generateToken(user), user);
 };
