8b2b8959f111d6fcd5706298ab2965dde7268001
Marcos Roberto Albrecht <marquinhos.beto@hotmail.com>
2024-05-22 14:26:24 -0300

    validate send password

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,17 @@
+import { NotFoundException } from '@exceptions/not-found-exception';
 import { UserModel } from '@modules/user/user.model';
 import { getUserByEmail } from '@modules/user/user.service';
+import { ValidatePassword } from '@utils/password';
 import { AuthDTO } from './dtos/auth.dto';
 
 export const validateAuth = async (authDTO: AuthDTO): Promise<UserModel> => {
   const user = await getUserByEmail(authDTO.email);
+
+  const isValidPassword = await ValidatePassword(authDTO.password, user.password);
+
+  if (!isValidPassword) {
+    throw new NotFoundException('User');
+  }
 
   return user;
 };
